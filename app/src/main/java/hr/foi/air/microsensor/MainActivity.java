43d5608d4f9a8066f822f44bda301adf3b6fa7a7
Darjan Baričević <dbaricevi@foi.hr>
2018-11-08 22:23:21 +0100

    Prijava korisnika
    Izmijenjena je logika kod prijave korisnika da ucitava podatke za prijavu iz forme. Nakon prijave, pokreće se aktivnost 'HomepageActivity'.

--- app/src/main/java/hr/foi/air/microsensor/MainActivity.java
+++ app/src/main/java/hr/foi/air/microsensor/MainActivity.java
@@ -1,58 +1,61 @@
 package hr.foi.air.microsensor;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.List;
 import java.util.Observable;
 import java.util.Observer;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 import hr.foi.air.webservice.Student;
 import hr.foi.air.webservice.StudentDataLoader;
 import hr.foi.air.webservice.StudentObservable;
 
 public class MainActivity extends AppCompatActivity implements Observer {
-
-    int counter = 0;
+    @BindView(R.id.mInputEmail) EditText inputEmail;
+    @BindView(R.id.mInputPassword) EditText inputPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         StudentObservable.getInstance().addObserver(this);
+        ButterKnife.bind(this);
     }
 
+    @OnClick(R.id.mLogin)
     public void onClick(View view)
     {
-        // ove stringove treba napuniti sa pravim podacima iz text view-a
-        String email = "dbaricevi@foi.hr";
-        String lozinka = "ticbra";
+        String email = inputEmail.getText().toString();
+        String lozinka = inputPassword.getText().toString();
         StudentDataLoader controller = new StudentDataLoader();
-        controller.start(email,lozinka);
+        controller.start(email, lozinka);
     }
 
-    //Funkcija koja prikazuje dobivene podatke tj. može se obrisati kasnije
-    private void setTextView(List<Student> list)
-    {
-        TextView textView = findViewById(R.id.textView);
-        Student student = null;
-        if(!list.isEmpty())
-        {
-            student = list.get(0);
-            textView.setText(student.getEmail());
-        }
-        else {
-            textView.setText("Neuspješna prijava");
-        }
-        counter++;
-    }
-
-    //U ovoj funkciji pozivati druge funkcije koje rade sa dohvaćenim podacima
+    // U ovoj funkciji pozivati druge funkcije koje rade sa dohvaćenim podacima
     @Override
     public void update(Observable o, Object arg) {
         List<Student> response = (List<Student>) arg;
-        setTextView(response);
+
+        Student currentUser = null;
+        if(!response.isEmpty())
+        {
+            currentUser = response.get(0);
+
+            Intent i = new Intent(MainActivity.this, HomepageActivity.class);
+            i.putExtra("CURRENT_USER", currentUser.getIme().toString());
+            startActivity(i);
+        }
+        else {
+            Toast.makeText(MainActivity.this, "Login failed.", Toast.LENGTH_SHORT).show();
+        }
     }
 }
